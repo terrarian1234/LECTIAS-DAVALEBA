b78e0ad9aab21acb8d1193479f3a145ecad3257e
terrarian1234 <164236579+terrarian1234@users.noreply.github.com>
2024-06-12 15:29:46 +0400

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,39 @@
+let Number =10
+switch (Number) {
+    case 10:
+    console.log('10')
+    break;
+    case 11:
+        console.log('11')
+    break;
+    case 12:
+    console.log('12')
+    break;
+    case 13:
+    console.log('13')
+    break;
+    case 14:
+    console.log('14')
+    break;
+    default:
+        console.log('15')
+    }
+
+
+
+
+
+
+
+
+
+
+
+    let saxelebi = ['goga', 'gaga', 'gega', 'luka', 'qeti']
+    saxelebi.push(0)
+    if (10<11) {
+        console.log(saxelebi [0])
+    }
+     if(15>10){
+        console.log(saxelebi [4])
+    }

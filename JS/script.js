3ef7ba019105859495414cb6c77b92b32665e7ff
terrarian1234 <164236579+terrarian1234@users.noreply.github.com>
2024-06-21 16:08:02 +0400

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,55 +1,152 @@
-let Number =10
-switch (Number) {
-    case 10:
-    console.log('10')
-    break;
-    case 11:
-        console.log('11')
-    break;
-    case 12:
-    console.log('12')
-    break;
-    case 13:
-    console.log('13')
-    break;
-    case 14:
-    console.log('14')
-    break;
-    default:
-        console.log('15')
+// let Number =10
+// switch (Number) {
+//     case 10:
+//     console.log('10')
+//     break;
+//     case 11:
+//         console.log('11')
+//     break;
+//     case 12:
+//     console.log('12')
+//     break;
+//     case 13:
+//     console.log('13')
+//     break;
+//     case 14:
+//     console.log('14')
+//     break;
+//     default:
+//         console.log('15')
+//     }
+
+
+
+
+
+
+
+
+
+
+//     let lu = ["levani","meko","giorgi","mamuka","gega"]
+
+//     lu.shift ()
+//     lu.push ("levani")
+    
+//     if (lu [4] == "levani") {
+//         console.log ("levani")
+//     } 
+    
+//     else if (lu [4] == "meko") {
+//         console.log ("meko")
+//     }
+    
+//     else if (lu [4] == "giorgi") {
+//         console.log ("giorgi")
+//     }
+    
+//     else if (lu [4] == "mamuka") {
+//         console.log ("mamuka")
+//     }
+    
+    
+//     else {
+//         console.log ("gega")
+//     }
+
+
+    // let students = ['Nika', 'Andria', 'Luka', 'Givi'];
+
+ 
+    // students.push('Saba');
+    
+    // students.pop()
+
+    // students.shift()
+
+    // students.unshift('Mari');
+    
+    // console.log(students)
+
+    // let myobject = {
+    //         model: 'iphone15',
+    //         releaseDate: '20/01/20',
+    //         price: '300'
+    // }
+    // console.log(myobject)
+
+
+    // let num1 ="10" 
+
+    // let num2 ="20"
+    // if (num1>num2) {
+    //         console.log("num1")
+    //         } else{
+    //             console.log("num1 naklebia num2 ze")
+    //         }
+
+
+
+//     let city = 'rustavi'  
+
+//         switch (city) {
+//     case 'tbilisi':
+//         console.log('tbilisshi 10 lari')
+//       break;
+//     case 'batumi':
+//         console.log('batumshi 20 lari')
+//       break;
+//     case 'kutaisi':
+//         console.log('kutaisshi 15 lari')
+//       break; 
+//       case 'rustavi':
+//         console.log('rustavshi 12 lari')
+//       break;   
+//     default:
+//       console.log('bakurianshi 25 lari')
+//   }
+  
+// for (let i = 0; i<11; i++) {
+//     console.log(i)
+// }
+
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// let student = {
+//     name: 'Andria',
+//     age: 20,
+//     subjects: ['Math', 'Science', 'English']
+// };
+
+// if (student.age > 18) {
+//     console.log('Adult'); S
+// } else {
+//     console.log('Minor');
+// }
+
+// for (let i = 0; i < student.subjects.length; i++) {
+//     console.log(student.subjects[i]);
+// }
+// for (let i = 0; i++;) {
+//     console.log(i)
+// }
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+console.log("Even numbers:");
+for (let i = 0; i < numbers; i++) {
+    if (numbers[i] % 2 === 0) {
+        console.log(numbers[i]);
     }
+}
 
 
+let sum = 0;
+for (let i = 0; i < numbers; i++) {
+    sum += numbers[i];
+}
 
+console.log("Sum of all numbers:", sum);
 
-
-
-
-
-
-
-    let lu = ["levani","meko","giorgi","mamuka","gega"]
-
-    lu.shift ()
-    lu.push ("levani")
-    
-    if (lu [4] == "levani") {
-        console.log ("levani")
-    } 
-    
-    else if (lu [4] == "meko") {
-        console.log ("meko")
-    }
-    
-    else if (lu [4] == "giorgi") {
-        console.log ("giorgi")
-    }
-    
-    else if (lu [4] == "mamuka") {
-        console.log ("mamuka")
-    }
-    
-    
-    else {
-        console.log ("gega")
-    }
